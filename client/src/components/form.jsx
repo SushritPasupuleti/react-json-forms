dae94c3e38cb3ac71c11238178ea7c13d8fdf6bc
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-03-26 16:50:18 +0530

    add json data to state that will be used by form

--- client/src/components/form.jsx
+++ client/src/components/form.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -9,43 +9,57 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
-  },
-  media: {
-    height: 140,
-  },
+	root: {
+		maxWidth: 345,
+	},
+	media: {
+		height: 140,
+	},
 });
 
 export default function FormCard() {
-  const classes = useStyles();
+	const classes = useStyles();
 
-  return (
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-            across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
-    </Card>
-  );
+	const [formData, setformData] = useState({
+		name: 'batman',
+		skills: [
+			{
+				name: 'ninjutsu',
+				profeciency: 'expert',
+			},
+			{
+				name: 'ninjutsu',
+				profeciency: 'expert',
+			},
+		],
+	});
+
+	return (
+		<Card className={classes.root}>
+			<CardActionArea>
+				<CardMedia
+					className={classes.media}
+					image="/static/images/cards/contemplative-reptile.jpg"
+					title="Contemplative Reptile"
+				/>
+				<CardContent>
+					<Typography gutterBottom variant="h5" component="h2">
+						Lizard
+					</Typography>
+					<Typography variant="body2" color="textSecondary" component="p">
+						Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all
+						continents except Antarctica
+					</Typography>
+				</CardContent>
+			</CardActionArea>
+			<CardActions>
+				<Button size="small" color="primary">
+					Share
+				</Button>
+				<Button size="small" color="primary">
+					Learn More
+				</Button>
+			</CardActions>
+		</Card>
+	);
 }
