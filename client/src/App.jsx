d7a7633dfcf287e4b02c32be0275cea7a9766cad
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-03-26 16:55:22 +0530

    add dark theme

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 import FormCard from './components/form';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+import green from '@material-ui/core/colors/green';
+
+const theme = createMuiTheme({
+	palette: {
+		primary: {
+			main: purple[500],
+		},
+		secondary: {
+			main: green[500],
+		},
+		type: 'dark',
+	},
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <FormCard></FormCard>
-      </header>
-    </div>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<FormCard></FormCard>
+				</header>
+			</div>
+		</ThemeProvider>
+	);
 }
 
 export default App;
